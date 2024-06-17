53d815dd1f2478f2eadce8fa97a8a3b8e0edf13d
tejas <bannore99tejas@gmail.com>
2024-06-17 23:00:24 +0530

    swagger added

--- src/main/java/com/demo/EmployeeCrudApplication.java
+++ src/main/java/com/demo/EmployeeCrudApplication.java
@@ -3,11 +3,22 @@ package com.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class EmployeeCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeCrudApplication.class, args);
+	}
+
+	public Docket employeeApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.demo"))
+				.build();
 	}
 
 }
